3f88acad48e7c54ad37ee97e0f45c43c13fe6125
thanasi2 <45300339+thanasi2@users.noreply.github.com>
2020-01-19 08:05:33 -0800

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,6 @@ import { validateURL } from './js/urlValidator'
 import { handleSubmit } from './js/formHandler'
 import './styles/header.scss'
 import './styles/form.scss'
-import './styles/footer.scss'
 import './styles/base.scss'
 
 export {
